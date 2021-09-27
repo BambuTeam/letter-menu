04fdf210efb02975fc6cde864a0bff13e1a0de8d
Luis Enrique Concuá <hola@bambu.team>
2021-09-27 00:11:43 -0600

    HOME

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import MenuCover from './assets/pages/menu-cover';
 import PatternSection from './assets/components/pattern';
+import MenuYBebidas from './assets/components/MenuyBebidas';
 
 
 function App() {
   return (
-    <><MenuCover /><PatternSection /></>
+    <><MenuCover /><PatternSection />
+    <MenuYBebidas />
+    <PatternSection />
+    </>
       );
 }
 
---
+++ src/assets/components/MenuyBebidas.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Col, Container, Row, Button } from 'react-bootstrap';
+import Icorestaurante from '../img/iconos/ico-restaurante.svg';
+import Icobebida from '../img/iconos/ico-bebidas.svg';
+
+
+
+
+const MenuYBebidas = () => {
+    return (
+
+        <div className="container_menu1">
+            <br />
+            <Container fluid className="Container_menu1">
+                <Row >
+
+                    <Col className="Btn_Style_1 border-rigth">
+                    <img src={Icobebida} alt="ico-restaurante" />
+
+
+                    <Button>
+                        BEBIDAS
+                    </Button>
+                    
+                    </Col>
+                    <Col className="Btn_Style_1">
+                    <img src={Icorestaurante} alt="ico-restaurante" />
+                    <Button>
+                        COMIDA
+                    </Button>
+                   
+
+
+                    
+                    
+
+                    
+                    </Col>
+                </Row>
+                
+            </Container>
+        </div>
+    )
+}
+export default MenuYBebidas;
