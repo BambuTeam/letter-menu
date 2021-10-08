7e0cc5c0c860a147c841f5907a26e019e7bb6faf
Luis Enrique Concuá <hola@bambu.team>
2021-10-07 23:05:45 -0600

    Ensaladas

--- src/assets/components/Entradas.jsx
+++ src/assets/components/Entradas.jsx
@@ -269,7 +269,113 @@ const EntradasMenu = () => {
         </div>
                 
             <br />
+
+
+            <div className="content_Entradas" id="Entradas_click">
+            <br />
+            <Container className="Container_Entradas">
+                <Row >
+                <Col xs={10} md={12} lg={10} className="Style_Entradas_List" >
+                <h1>ENSALADAS</h1>
+                    </Col>
+                </Row>
+
+                <div className="Space_Style"></div>
+
+                {/* D E S A Y U N O S LIST */}
+
+                <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Entradas_List" >
+                <h2>Sopa de Cebolla Bordeaux   </h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Entradas_List F_rigth" >
+                <h2>Q.80</h2>
+                </Col>
+
+                </Row>
+                <Row>
+                <Col xs={12} md={12} lg={12} className="Style_Entradas_List" >
+                    <p>
+                    Receta original hecha a base de cebolla con vino bordeaux y <br /> sherry, gratinado con
+                    queso gruyere.		
+                    </p>
+                    </Col>
+                </Row>
+                </Container>
+                
+                {/* E N D D E S A Y U N O S LIST */}
+
+                 {/* D E S A Y U N O S LIST */}
+
+                 <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Entradas_List" >
+                <h2>Sopa de Frijoles </h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Entradas_List F_rigth" >
+                <h2>Q.55</h2>
+                </Col>
+
+                </Row>
+                <Row>
+                <Col xs={12} md={12} lg={12} className="Style_Entradas_List" >
+                    <p>
+                    Nuestra sopa de frijoles típicamente chapina.
+
+                    </p>
+                    </Col>
+                </Row>
+                </Container>
+                
+                {/* E N D D E S A Y U N O S LIST */}
+
+                {/* D E S A Y U N O S LIST */}
+
+                <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Entradas_List" >
+                <h2>Sopa de Tortilla </h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Entradas_List F_rigth" >
+                <h2>Q.65</h2>
+                </Col>
+
+                </Row>
+                <Row>
+                <Col xs={12} md={12} lg={12} className="Style_Entradas_List" >
+                    <p>
+                    Original receta de la finca, acompañada de chips de tortillas, <br /> queso fresco
+                    trozos de Aguacate y cilantro picado.							
+                    </p>
+                    <p>
+                    Con pollo	Q75.00
+                    </p>
+                    </Col>
+                </Row>
+                </Container>
+                
+                
+            </Container>
+            <div>
+            <PatternMenu />
+            </div>
+
+            
+
+            
+                
+            <br />
         </div>
+                
+            <br />        
+
+
+        </div>
+
+
+
+        
 
         
         
