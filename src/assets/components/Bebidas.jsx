2a71b0fdcf12f4c1be965b0d7c36a98ed93734dc
Luis Enrique Concuá <hola@bambu.team>
2021-10-09 15:11:35 -0600

    Bebidas Calientes

--- src/assets/components/Bebidas.jsx
+++ src/assets/components/Bebidas.jsx
@@ -17,13 +17,10 @@ const Bebidas = () => {
                 <Row >
 
                     <Col xs={12} md={12} lg={12} className="Btn_Style_2 ">
-                    <h1>DESAYUNOS</h1>
+                    <h1>CALIENTES</h1>
                     </Col>
                     <div>
-                    <h3>Disponible hasta las 11:00 a.m.</h3>
-                
-                    <h4>Incluyen jugo natural pequeño, café de la finca o té,<br />
-                        pan o tortillas, mantequilla y mermelada.</h4>
+                    <h3>Preparados con nuestro café de la Finca.</h3>
                     </div>
                 </Row>
 
@@ -34,184 +31,165 @@ const Bebidas = () => {
                 <Container>
                 <Row>
                 <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
-                <h2>Plato de Frutas</h2>
-                </Col>
-                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
-                <h2>Q.65</h2>
-                </Col>
-
-                </Row>
-                <Row>
-                <Col xs={12} md={12} lg={12} className="Style_Desayuno_List" >
-                    <p>
-                    Frutas de la temporada acompañadas de yogurt natural, <br />
-                    granola y miel.
-                    </p>
-                    </Col>
-                </Row>
-                </Container>
-                
-                {/* E N D D E S A Y U N O S LIST */}
-
-                 {/* D E S A Y U N O S LIST */}
-
-                 <Container>
-                <Row>
-                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
-                <h2>Típico de la Finca</h2>
-                </Col>
-                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
-                <h2>Q.75</h2>
-                </Col>
-
-                </Row>
-                <Row>
-                <Col xs={12} md={12} lg={12} className="Style_Desayuno_List" >
-                    <p>
-                    Dos huevos al gusto, acompañados de salsa ranchera, frijoles <br />
-                    queso blanco y plátanos fritos.
-                    </p>
-                    </Col>
-                </Row>
-                </Container>
-                
-                {/* E N D D E S A Y U N O S LIST */}
-
-                {/* D E S A Y U N O S LIST */}
-
-                <Container>
-                <Row>
-                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
-                <h2>Desayuno Finquero</h2>
-                </Col>
-                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
-                <h2>Q.110</h2>
-                </Col>
-
-                </Row>
-                <Row>
-                <Col xs={12} md={12} lg={12} className="Style_Desayuno_List" >
-                    <p>
-                    Dos huevos al gusto montados sobre 4 oz. de carne, acompañados <br />
-                    de salsa ranchera, frijoles, queso fresco y plátanos fritos.
-                    </p>
-                    </Col>
-                </Row>
-                </Container>
-                
-                {/* E N D D E S A Y U N O S LIST */}
-
-                {/* D E S A Y U N O S LIST */}
-
-                <Container>
-                <Row>
-                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
                 <h2>Americano</h2>
                 </Col>
                 <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
-                <h2>Q.75</h2>
+                <h2>Q.15</h2>
                 </Col>
 
-                </Row>
-                <Row>
-                <Col xs={12} md={12} lg={12} className="Style_Desayuno_List" >
-                    <p>
-                    Dos huevos al gusto acompañados de tocino, jamón o <br />
-                    salchicha y papas salteadas con hierbas.
-                    </p>
-                    </Col>
                 </Row>
                 </Container>
                 
                 {/* E N D D E S A Y U N O S LIST */}
-
                  {/* D E S A Y U N O S LIST */}
 
                  <Container>
                 <Row>
                 <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
-                <h2>Divorciados</h2>
+                <h2>Capuccino</h2>
                 </Col>
                 <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
-                <h2>Q.80</h2>
+                <h2>Q.20</h2>
                 </Col>
 
-                </Row>
-                <Row>
-                <Col xs={12} md={12} lg={12} className="Style_Desayuno_List" >
-                    <p>
-                    Dos huevos fritos con salsa verde y salsa roja sobre <br />
-                    sincronizada de jamón y queso con frijoles y plátanos fritos.
-                    </p>
-                    </Col>
                 </Row>
                 </Container>
                 
                 {/* E N D D E S A Y U N O S LIST */}
-
                 {/* D E S A Y U N O S LIST */}
 
                 <Container>
                 <Row>
                 <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
-                <h2>Benedictinos</h2>
+                <h2>Latté</h2>
                 </Col>
                 <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
-                <h2>Q.80</h2>
+                <h2>Q.20</h2>
                 </Col>
 
-                </Row>
-                <Row>
-                <Col xs={12} md={12} lg={12} className="Style_Desayuno_List" >
-                    <p>
-                    Dos huevos poshé con jamón virginia, sobre pan muffin y salsa <br />
-                    holandes acompañado de tomates asados y papas salteadas <br />
-                    con hierbas.
-                    </p>
-                    <p>
-                    Con salmón Q.90
-                    </p>
-                    </Col>
                 </Row>
                 </Container>
                 
                 {/* E N D D E S A Y U N O S LIST */}
-
                 {/* D E S A Y U N O S LIST */}
 
                 <Container>
                 <Row>
                 <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
-                <h2>Croque Madame</h2>
+                <h2>Mocca</h2>
                 </Col>
                 <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
-                <h2>Q.80</h2>
+                <h2>Q.22</h2>
                 </Col>
 
-                </Row>
-                <Row>
-                <Col xs={12} md={12} lg={12} className="Style_Desayuno_List" >
-                    <p>
-                    Sándwich francés elaborado con pan de molde, jamón virginia y <br />
-                    queso gruyere gratinado al horno cubierto con huevo frito. <br />
-                    Acompañado de papas salteadas con hierbas.
-                    </p>
-                    </Col>
                 </Row>
                 </Container>
                 
                 {/* E N D D E S A Y U N O S LIST */}
-                {/* E N D D E S A Y U N O S LIST - PAGE2 */}
+                {/* D E S A Y U N O S LIST */}
 
+                <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
+                <h2>Espresso</h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
+                <h2>Q.15</h2>
+                </Col>
+
+                </Row>
+                </Container>
                 
+                {/* E N D D E S A Y U N O S LIST */}
+                {/* D E S A Y U N O S LIST */}
 
+                <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
+                <h2>Cortado</h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
+                <h2>Q.20</h2>
+                </Col>
 
+                </Row>
+                </Container>
+                
+                {/* E N D D E S A Y U N O S LIST */}
+                {/* D E S A Y U N O S LIST */}
 
+                <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
+                <h2>Macchiato	</h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
+                <h2>Q.20</h2>
+                </Col>
 
+                </Row>
+                </Container>
+                
+                {/* E N D D E S A Y U N O S LIST */}
+                {/* D E S A Y U N O S LIST */}
 
+                <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
+                <h2>Té</h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
+                <h2>Q.16</h2>
+                </Col>
 
+                </Row>
+                </Container>
+                
+                 {/* D E S A Y U N O S LIST */}
 
+                 <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
+                <h2>Té Chai	</h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
+                <h2>Q.25</h2>
+                </Col>
 
+                </Row>
+                </Container>
+                
+                {/* E N D D E S A Y U N O S LIST */}
+                  {/* D E S A Y U N O S LIST */}
+
+                  <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
+                <h2>Variedad de Infusiones</h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
+                <h2>Q.25</h2>
+                </Col>
+
+                </Row>
+                </Container>
+                
+                {/* E N D D E S A Y U N O S LIST */}
+                 {/* D E S A Y U N O S LIST */}
+
+                 <Container>
+                <Row>
+                <Col xs={10} md={12} lg={10} className="Style_Desayuno_List" >
+                <h2>Chocolate caliente</h2>
+                </Col>
+                <Col xs={2} md={12} lg={2} className="Style_Desayuno_List F_rigth" >
+                <h2>Q.20</h2>
+                </Col>
+
+                </Row>
+                </Container>
+                
+                {/* E N D D E S A Y U N O S LIST */}
 
 
 
